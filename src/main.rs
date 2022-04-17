a62ef8488b02133ae98cf06920e647fb0bfdcd37
Konstantin Perikov <pericov.ko@gmail.com>
2022-04-17 17:05:27 +0100

    add waverform files and reader

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use image::{ImageBuffer, Rgb, RgbImage};
 use image::imageops::{flip_vertical_in_place};
-use renderer::{draw_line};
+use renderer::{draw_line, read_waveform_obj_file};
 
 
 fn main() {
+    read_waveform_obj_file("resources/teapot.obj");
     let tga_red = Rgb([255, 0, 0]);
     let tga_white = Rgb([255, 255, 255]);
     let mut image: RgbImage = ImageBuffer::new(100, 100);
-    // *image.get_pixel_mut(52, 41) = tga_red;
-    // draw_line(0, 0, 100, 100, &mut image, tga_red);
     draw_line(13, 20, 80, 40, &mut image, tga_white);
     draw_line(20, 13, 40, 80, &mut image, tga_red);
     draw_line(80, 40, 13, 20, &mut image, tga_red);
--- src/renderer.rs
+++ src/renderer.rs
@@ -1,4 +1,24 @@
 use image::{Rgb, RgbImage};
+use std::fs::File;
+use std::io;
+use std::io::{BufRead};
+use std::path::Path;
+use io::BufReader;
+
+pub fn read_waveform_obj_file(filename: &str) {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    let lines = reader.lines();
+    for line in lines {
+        let line = line.unwrap();
+        if line.starts_with("f") {
+            println!("{}", line);
+        } else if line.starts_with("v") {
+            println!("{}", line);
+        }
+
+    }
+}
 
 pub fn draw_line(mut x1: i32, mut y1: i32, mut x2: i32, mut y2: i32, image: &mut RgbImage, colour: Rgb<u8>) {
     let mut steep: bool = false;
@@ -12,14 +32,22 @@ pub fn draw_line(mut x1: i32, mut y1: i32, mut x2: i32, mut y2: i32, image: &mut
         std::mem::swap(&mut y1, &mut y2);
     }
 
+    let dx = x2 - x1;
+    let derror2 = i32::abs(y2 - y1) * 2;
+    let mut error2 = 0;
+    let mut y = y1;
     for x in x1..x2 {
-        let t = (x - x1) as f32 / (x2 - x1) as f32;
-        let y = y1 as f32 * (1.0 - t) + y2 as f32 * t;
         if steep {
             *image.get_pixel_mut(y as u32, x as u32) = colour;
         } else {
             *image.get_pixel_mut(x as u32, y as u32) = colour;
         }
+        error2 += derror2;
+        if error2 > dx {
+            y += if y2 > y1 { 1 } else { -1 };
+            error2 -= dx * 2;
+        }
+
     }
 }
 
